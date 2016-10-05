6cac55b9fdb78258ffd0c717ff7d026c1750f1bc
jetsonhacks <jim@jetsonhacks>
2016-10-05 11:23:59 -0700

    Fix applyUVCPatch patch flag

--- applyUVCPatch.sh
+++ applyUVCPatch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Patch UVC RealSense camera format changes
 cd /usr/src/kernel
-sudo patch -p1 i- $HOME/librealsense/scripts/realsense-camera-formats.patch
+sudo patch -p1 -i $HOME/librealsense/scripts/realsense-camera-formats.patch
 
